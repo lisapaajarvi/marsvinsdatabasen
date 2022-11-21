8e7944dcd35ccb76a90e2b4921e2b4b8f6e635d2
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2022-11-21 15:21:08 +0100

    name input and toggle

--- pages/components/guineaPigNotes.tsx
+++ pages/components/guineaPigNotes.tsx
@@ -1,14 +1,28 @@
 import styles from '../../styles/Home.module.css'
+import { useState } from 'react'
 
-export default function guineaPigNotes() {
+export default function GuineaPigNotes() {
+    const [isInputVisible, setInputVisible] = useState(false);
+    const inputToggle = () => {
+        setInputVisible(!isInputVisible)
+    }
     return (
         <>
             <div className={styles.btnContainer}>
                 <button
-                    className={styles.button}>
+                    className={styles.button}
+                    onClick={inputToggle}>
                     Add a New Guinea Pig Note
                 </button>
             </div>
+            {isInputVisible ? (
+                <div className={styles.inputContainer}>
+                    <input placeholder='Enter Guinea Pig Name'
+                    className={styles.input}/>
+                </div>
+            ) : (
+                <></>
+            )}
         </>
     )
 }
