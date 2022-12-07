05581a48abca80681f466ead24915c054a626a25
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2022-12-08 00:31:10 +0100

    auth function

---
+++ hooks/useAuth.ts
@@ -0,0 +1,18 @@
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { app } from "../firebaseConfig";
+
+const useAuth = () => {
+    const [user, setUser] = useState({} as User | null);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const auth = getAuth(app);
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+        setIsLoggedIn(user && user.uid ? true : false);
+        setUser(user);
+        });
+    });
+    return { user, isLoggedIn };
+};
+
+export default useAuth;
